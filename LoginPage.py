91cc715437b5e635546d398789114f4f560798a9
Balamurugan <84506577+Bala00300@users.noreply.github.com>
2022-05-22 15:00:03 +0530

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -1,9 +1,9 @@
 class LoginPage:
     # Login Page
-    textbox_email_xpath = "//*[@id=root]/div/div/div[1]/div[2]/div/div[1]/form/div[1]/div/input"
-    textbox_password_xpath = "//*[@id=root]/div/div/div[1]/div[2]/div/div[1]/form/div[2]/div/input"
-    button_login_xpath = "//*[@id=root]/div/div/div[1]/div[2]/div/div[1]/form/button"
-    button_logout_xpath = "//*[@id=root]/nav/div/div/div/div/button[2]"
+    textbox_email_xpath = "/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/div[1]/div/input"
+    textbox_password_xpath = "/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/div[2]/div/input"
+    button_login_xpath = "/html/body/div/div/div/div[1]/div[2]/div/div[1]/form/button"
+    button_logout_xpath = "/html/body/div/nav/div/div/div/div/button[2]"
 
     def __init__(self,driver):
         self.driver=driver
@@ -19,5 +19,3 @@ class LoginPage:
     def clickLogin(self):
         self.driver.find_element_by_xpath(self.button_login_xpath).click()
 
-    def clickLogout(self):
-        self.driver.find_element_by_link_text(self.link_logout_linktext).click()
